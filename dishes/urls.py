f94b8018c67203d39c82ce1effe29ffd0d056f0f
Aziz Harrington <azizharrington@gmail.com>
2013-10-20 16:13:29 -0500

    login required for delete, update, create

--- dishes/urls.py
+++ dishes/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from dishes.views import list_view, detail, new, edit, delete
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     url(r'^list/?', list_view, name="list"),
     url(r'^detail/(?P<pk>\d+)/?$', detail, name="detail"),
-    url(r'^new/?', new, name="new"),
-    url(r'^edit/(?P<pk>\d+)/?$', edit, name="edit"),
-    url(r'^delete/(?P<pk>\d+)/?$', delete, name="delete"), 
+    url(r'^new/?', login_required(new), name="new"),
+    url(r'^edit/(?P<pk>\d+)/?$', login_required(edit), name="edit"),
+    url(r'^delete/(?P<pk>\d+)/?$', login_required(delete), name="delete"), 
 )
