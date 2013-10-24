130a3e801e323a0fb51bd64306b9f40e9005733a
Aziz Harrington <azizharrington@gmail.com>
2013-10-23 21:33:50 -0500

    basic tests for dishes models, views

--- dishes/tests.py
+++ dishes/tests.py
@@ -5,12 +5,36 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from time import time
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from .models import Dish, get_upload_file_name
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class DishTest(TestCase):
+    def create_dish(self, name="cake", cuisine="dessert",
+                    flavors="sweet", prep_time="30", comments="delicious"):
+        return Dish.objects.create(name=name,
+                                   cuisine=cuisine,
+                                   flavors=flavors,
+                                   prep_time=prep_time,
+                                   comments=comments)
+
+    def test_dish_creation(self):
+        d = self.create_dish()
+        self.assertTrue(isinstance(d, Dish))
+        self.assertEqual(d.__unicode__(), d.name)
+
+    def test_get_upload_file_name(self):
+        filename = "cake.jpg"
+        path = "uploaded/%s_%s" % (str(time()).replace('.', '_'), filename)
+        created_path = get_upload_file_name(self, filename)
+        self.assertEqual(path, created_path)
+
+    def test_dish_list_view(self):
+        d = self.create_dish()
+        url = reverse('list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(d.name, response.content)
