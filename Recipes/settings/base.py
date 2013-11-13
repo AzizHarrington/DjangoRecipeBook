d1668b6ab27aed0552eb5d70a36422e2f25fe740
Aziz Harrington <azizharrington@gmail.com>
2013-11-12 22:37:55 -0600

    s3 setting

--- Recipes/settings/base.py
+++ Recipes/settings/base.py
@@ -177,5 +177,5 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-    STATIC_URL = S3_URL + '/static/'
-    MEDIA_URL = S3_URL + '/media/'
+    STATIC_URL = S3_URL + 'static/'
+    MEDIA_URL = S3_URL + 'media/'
